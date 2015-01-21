72b9d2e49799e19370a5d0629629940bea737ec8
jasperhowald <jazdrum3r@yahoo.com>
2015-01-20 17:58:55 -0700

    particleFilter Addition
    Added a particleFilter process to the processing of the image.
    particleFilter’s syntax proved a challenge.

--- src/org/usfirst/frc/team4068/robot/subsystems/Vision.java
+++ src/org/usfirst/frc/team4068/robot/subsystems/Vision.java
@@ -2,6 +2,8 @@ package org.usfirst.frc.team4068.robot.subsystems;
 
 import com.ni.vision.NIVision;
 import com.ni.vision.NIVision.Image;
+import com.ni.vision.NIVision.ParticleFilterCriteria2;
+import com.ni.vision.NIVision.MeasurementType;
 
 import edu.wpi.first.wpilibj.image.BinaryImage;
 import edu.wpi.first.wpilibj.image.ColorImage;
@@ -12,6 +14,9 @@ public class Vision extends AxisCamera{
 		super(cameraHost);
 		// TODO Auto-generated constructor stub
 	}
+	
+	ParticleFilterCriteria2 criteria = new ParticleFilterCriteria2();
+	
 	Image frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
 	//Need to find correct IP address for the camera.
 	//I'm pretty sure that this just sets the camera to the name of camera.
@@ -21,12 +26,13 @@ public class Vision extends AxisCamera{
 		BinaryImage thresholdImage = null;
 		BinaryImage bigObjectsImage = null;
 		BinaryImage convexHullImage = null;
+		BinaryImage filteredImage = null;
 		try{
 			image = this.getImage();
 			thresholdImage = image.thresholdRGB(0, 45, 25, 255, 0, 45); //Filters out most of the non-green. We use green lights, so the green should  be what we need to see.
 			bigObjectsImage = thresholdImage.removeSmallObjects(false, 2); //Filters small particles.
 			convexHullImage = bigObjectsImage.convexHull(false); //The video said that this fills the rectangles or something like that. I don't understand why.
-			
+			filteredImage = convexHullImage.particleFilter(null);
 		}catch(Exception ex){
 			
 		}finally{
