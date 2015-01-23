311384d2011e411467be495cc5b7defc7cbb57fb
jasperhowald <jazdrum3r@yahoo.com>
2015-01-22 17:26:11 -0700

    Check if filteredImage is created.
    The imageRetrieval() thingy returns true if filteredImage DNE null.

--- src/org/usfirst/frc/team4068/robot/subsystems/Vision.java
+++ src/org/usfirst/frc/team4068/robot/subsystems/Vision.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team4068.robot.subsystems;
 
+import java.nio.ByteBuffer;
+
 import com.ni.vision.NIVision;
 import com.ni.vision.NIVision.Image;
 import com.ni.vision.NIVision.ParticleFilterCriteria2;
@@ -14,14 +16,18 @@ public class Vision extends AxisCamera{
 		super(cameraHost);
 		// TODO Auto-generated constructor stub
 	}
-	
-	ParticleFilterCriteria2 criteria = new ParticleFilterCriteria2();
+
+	/*AxisCamera camera = new AxisCamera("10.40.68.11");
 	
 	Image frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
+	
+	ParticleFilterCriteria2[] criteria = {null};*/
+
+	
 	//Need to find correct IP address for the camera.
 	//I'm pretty sure that this just sets the camera to the name of camera.
 	
-	public void imageRetrieval() {
+	public boolean imageRetrieval() {
 		ColorImage image = null;
 		BinaryImage thresholdImage = null;
 		BinaryImage bigObjectsImage = null;
@@ -32,11 +38,20 @@ public class Vision extends AxisCamera{
 			thresholdImage = image.thresholdRGB(0, 45, 25, 255, 0, 45); //Filters out most of the non-green. We use green lights, so the green should  be what we need to see.
 			bigObjectsImage = thresholdImage.removeSmallObjects(false, 2); //Filters small particles.
 			convexHullImage = bigObjectsImage.convexHull(false); //The video said that this fills the rectangles or something like that. I don't understand why.
-			filteredImage = convexHullImage.particleFilter(null);
+			filteredImage = convexHullImage.particleFilter(criteria); //Looks for rectangular particles I think.
 		}catch(Exception ex){
 			
 		}finally{
 			
 		}
+		if(filteredImage != null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public boolean isToteInView() {
+		return false;
 	}
 }
