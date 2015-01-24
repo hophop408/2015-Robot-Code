d111be67efb7cc00d9aecaed6a6ffcd505feb6c7
hophop408 <hophop408@gmail.com>
2015-01-24 10:18:24 -0700

    Update Autonomous.java

--- src/org/usfirst/frc/team4068/robot/code/Autonomous.java
+++ src/org/usfirst/frc/team4068/robot/code/Autonomous.java
@@ -10,10 +10,50 @@ import org.usfirst.frc.team4068.robot.lib.References;
 
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Timer;
+import java.awt.Robot;
+import edu.wpi.first.wpilibj.TalonSRX;
+import edu.wpi.first.wpilibj.smartdashboard.AnalogChannel;
+import edu.wpi.first.wpilibj.smartdashboard.RobotDrive;
+import edu.wpi.first.wpilibj.smartdashboard.Talon;
+import edu.wpi.first.wpilibj.smartdashboard.Victor;
 
 public class Autonomous implements Runnable{
     private String thread;
     private boolean run = false;
+    
+public class SmartDashboard extends Object {{
+	
+	    Talon drivetrainLeft = new Talon(1);
+        
+	    LiveWindow.addActuator("Drive train Left", (Talon) drivetrainLeft);
+        
+	    Talon drivetrainRight = new Talon(1);
+    	
+	    LiveWindow.addActuator("Drive train Right", (Talon) drivetrainRight);
+        
+	    RobotDrive drivetrainRobotDrive = new RobotDrive(drivetrainLeft, drivetrainRight);
+        
+	    RobotDrive.drivetrainRobotDrive.setSafetyEnabled(false);
+        
+	    RobotDrive.drivetrainRobotDrive.setExpiration(0.1);
+        
+	    RobotDrive.drivetrainRobotDrive.setSensitivity(0.5);
+        
+	    RobotDrive.drivetrainRobotDrive.setMaxOutput(1.0);
+        
+	    Victor elevatorMotor = new Victor(1);
+	    
+	    LiveWindow.addActuator("Elevator", (Victor) elevatorMotor);
+        
+	    AnalogChannel elevatorHeight = new AnalogChannel(1);
+	    
+	    LiveWindow.addSensor("Elevator", elevatorHeight);
+        
+	    Victor gripMotor = new Victor(1);
+	    
+	    LiveWindow.addActuator("Grip", "Motor", (Victor) gripMotor);
+
+}}
     
     public Autonomous(String thread){
         this.thread = thread;
